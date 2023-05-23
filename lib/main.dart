916f63d19454dfe9ccd3242e3b3542762c355810
AdamMbule <adammbule98@gmail.com>
2023-05-23 22:11:27 +0300

    Dicey

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Text('Dicey'),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
