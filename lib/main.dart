e9d9df3296a1e8c16f25067d79cf257478476fd3
AdamMbule <adammbule98@gmail.com>
2023-05-23 22:29:02 +0300

    Expanded class

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,10 @@ void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.blue,
         appBar: AppBar(
           title: Text('Dicey'),
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.blue,
         ),
         body: DicePage(),
       ),
@@ -18,6 +18,25 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: [
+        Expanded(
+          flex:1,
+          child: Image(
+            width: 200.0,
+            image: AssetImage(
+                'images/dice1.png'), //make sure the image does not overflow dice 1
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Image(
+            width: 200.0,
+            image: AssetImage(
+                'images/dice1.png'), //dice 2
+          ),
+        ),
+      ],
+    );
   }
 }
